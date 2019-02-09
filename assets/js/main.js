16d21fca25b9a83336a092eac17f1cfe07362fce
pedropcruz <pedropcruz@outlook.pt>
2019-02-09 01:41:58 +0000

    finish slider styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,27 @@
 
 	'use strict';
 
+	var $slickElement = $('.slider-wrapper'),
+			$status = $('.slider-paging');
+
 	function applySlick () {
-		$('.slider-wrapper').slick({
+		$slickElement.slick({
 			arrows: true,
 			dots: true,
+			prevArrow: $('.slider-buttons .prev'),
+			nextArrow: $('.slider-buttons .next')
+		});
+	}
+
+	function currentAndTotalSlider() {
+		$status.text('1 / ' + $('.slick-slide:not(.slick-cloned)').length)
+		$slickElement.on('init afterChange', function(e, slick, currentSlide){
+			var i = (currentSlide ? currentSlide : 0) + 1;
+			$status.text(i + ' / ' + slick.slideCount);
 		});
 	}
 
 	applySlick();
+	currentAndTotalSlider();
 
 }($));
